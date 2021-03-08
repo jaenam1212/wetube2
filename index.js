d3cb57c5a7e1b7ddddef0b5d09cdce40917a384f
jaenam1212 <jaenam1212@gmail.com>
2021-03-08 19:59:58 +0900

    fuck

--- index.js
+++ index.js
@@ -1,10 +1,23 @@
-const express = require('express')
+import express from "express";
 const app = express()
 
 const PORT = 4000;
 
-function handleListening(){
-    console.log(`Listening on: http://localhost:${PORT}`);
+const handleListening = () => console.log(`Listening on: http://localhost:${PORT}`);
+
+const handleHome = (req, res, next) => res.send("hello from ass");
+
+const handleProfile = (req, res, next) => res.send("U R on my profile");
+
+const betweenHome = (req, res, next) => {
+    console.log("between");
+    next();
 }
+
+app.use(betweenHome);
+
+app.get("/", handleHome);
+
+app.get("/profile", handleProfile);
 
 app.listen(PORT, handleListening);
