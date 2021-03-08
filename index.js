ee0bcc1be40e02c3b021ffd84a828f87e68ea473
jaenam1212 <jaenam1212@gmail.com>
2021-03-08 20:25:19 +0900

    fuuck

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 import express from "express";
+import morgan from "morgan";
+import helmat from "helmet";
+import cookieParser from "cookie-parser";
+import bodyParser from "body-parser";
+
 const app = express()
 
 const PORT = 4000;
@@ -14,7 +19,13 @@ const betweenHome = (req, res, next) => {
     next();
 }
 
-app.use(betweenHome);
+app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(helmat());
+app.use(morgan("dev"));
+app.use(morgan("tiny"));
+
 
 app.get("/", handleHome);
 
